03b3f3c0ca0cd82698b1067ca8337f6b0a0a5992
mingir2 <mingir2@illinois.edu>
2020-04-09 10:43:46 -0500

    Handle multiple lines

--- src/comps/Article.js
+++ src/comps/Article.js
@@ -96,7 +96,7 @@ const fetchQuill = (payload) => {
         })
         .then((json) => {
             if (json) {
-                return json[0];
+                return json;
             } else {
                 console.log({ json: json });
                 return null;
@@ -140,10 +140,6 @@ class Article extends React.Component {
     };
 
     updateView = (json, range) => {
-        const orig = json.original;
-        const pps = json.paraphrases[0];
-        const segs = pps.segs;
-
         let start = range.startContainer.parentElement;
         let end = range.endContainer.parentElement.nextElementSibling;
 
@@ -152,33 +148,42 @@ class Article extends React.Component {
             start = range.startContainer.nextElementSibling;
         }
 
-        for (let i = 0; i < segs.length; i++) {
-            const seg = segs[i];
+        json.forEach((data) => {
+            const orig = data.original;
+            const pps = data.paraphrases[0];
+            const segs = pps.segs;
 
-            if (start) {
-                // Check for punctuation
-                if (i < segs.length - 1) {
-                    const nextSeg = segs[i + 1].text;
-                    const hasPunct = removePunct(nextSeg) === "";
-                    var punct = hasPunct ? nextSeg : "";
-                    if (hasPunct) i++;
-                }
+            for (let i = 0; i < segs.length; i++) {
+                const seg = segs[i];
 
-                // Set attributes and properties according to each seg
-                if (orig.toLowerCase().includes(seg.text.toLowerCase())) {
-                    if (seg.alts.length) {
-                        start.setAttribute("class", "article-unchanged");
+                if (start) {
+                    // Check for punctuation
+                    if (i < segs.length - 1) {
+                        const nextSeg = segs[i + 1].text;
+                        const hasPunct = removePunct(nextSeg) === "";
+                        var punct = hasPunct ? nextSeg : "";
+                        if (hasPunct) i++;
                     }
-                } else {
-                    start.setAttribute("class", "article-word");
-                }
-                start.innerHTML = `${seg.text}${punct}`;
-                start.alts = seg.alts.map((a) => a.text);
-                start.onclick = this.showAlts;
 
-                start = start.nextElementSibling;
+                    // Set attributes and properties according to each seg
+                    if (orig.toLowerCase().includes(seg.text.toLowerCase())) {
+                        if (seg.alts.length) {
+                            start.setAttribute("class", "article-unchanged");
+                        }
+                    } else {
+                        start.setAttribute("class", "article-word");
+                    }
+                    start.innerHTML = `${seg.text}${punct}`;
+                    start.alts = seg.alts.map((a) => a.text);
+                    start.onclick = this.showAlts;
+
+                    start = start.nextElementSibling;
+                } else {
+                    console.log("not enough elements");
+                }
             }
-        }
+        });
+
         // Empty unused ranges
         while (start !== end && start) {
             start.innerHTML = "";
